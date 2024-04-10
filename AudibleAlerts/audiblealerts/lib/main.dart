8f1f8b3880dd14830611d3440a1dc4ae00ed73fe
Aswin-K08 <aswinkala1999@gmail.com>
2024-04-10 15:31:03 +0530

    Update main.dart

--- AudibleAlerts/audiblealerts/lib/main.dart
+++ AudibleAlerts/audiblealerts/lib/main.dart
@@ -2,6 +2,9 @@ import "package:audiblealerts/addtask_page.dart";
 import "package:flutter/material.dart";
 import "package:audiblealerts/appbar_style.dart";
 import "package:audiblealerts/model/ListDate.dart";
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 void main() async {
   runApp(
@@ -9,18 +12,8 @@ void main() async {
   );
 }
 
-ThemeMode? selectedTheme;
-
-class AudibleAlerts extends StatefulWidget {
-  const AudibleAlerts({super.key, selectedTheme});
-
-  @override
-  State<AudibleAlerts> createState() {
-    return _AudibleAlertsState();
-  }
-}
-
-class _AudibleAlertsState extends State<AudibleAlerts> {
+class AudibleAlerts extends StatelessWidget {
+  const AudibleAlerts({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: MyHomePage());
@@ -65,5 +58,22 @@ class MyHomePage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _insertData() async {
+    int result = await dbHelper.insertData(
+        textfieldValue.toString(),
+        DateTime.now().toIso8601String(),
+        dropdownvalue2.toString(),
+        dropdownvalue.toString());
+    print('Inserted row id: $result');
+  }
+
+  void _retrieveData() async {
+    List<Map<String, dynamic>> rows = await dbHelper.getData();
+    rows.forEach((row) {
+      print(
+          'String value: ${row['stringValue']}, DateTime value: ${row['dateTimeValue']}');
+    });
   }
 }
